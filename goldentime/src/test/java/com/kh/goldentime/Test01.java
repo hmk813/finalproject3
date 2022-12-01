9da20c7ad0ec106909d2f28d4204f2725d7920b0
hmk813 <moonkyus@hanmail.net>
2022-12-01 16:07:27 +0900

    푸시테스트

---
+++ goldentime/src/test/java/com/kh/goldentime/Test01.java
@@ -0,0 +1,13 @@
+package com.kh.goldentime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test01 {
+
+	@Test
+	public void test01() {
+		System.out.println(11);
+	}
+}

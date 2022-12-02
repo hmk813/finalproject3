1caf757bc215dad0fb38815140e7df5affca0721
hyunganom <dlguswo0504@naver.com>
2022-12-02 11:01:04 +0900

    깃허브 연동 테스트

---
+++ goldentime/src/test/java/com/kh/goldentime/HyunJaeTest.java
@@ -0,0 +1,13 @@
+package com.kh.goldentime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HyunJaeTest {
+
+	@Test
+	public void test01() {
+		System.out.println("test");
+	}
+}

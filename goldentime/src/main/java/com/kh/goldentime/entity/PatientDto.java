104192301beb00f2cde9353e9d432881f123203b
scotty1003 <scotty1003@naver.com>
2022-12-05 13:34:03 +0900

    환자 등록 (접수)

--- goldentime/src/main/java/com/kh/goldentime/entity/PatientDto.java
+++ goldentime/src/main/java/com/kh/goldentime/entity/PatientDto.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class PatientDto {
-	private int patient_no;
+	private int patientNo;
 	private Date patientBirth;
-	private String patientName, patientGender; 
-
+	private String patientName, patientGender, patientPhone,
+	patientAddress, patientBlood; 
 }

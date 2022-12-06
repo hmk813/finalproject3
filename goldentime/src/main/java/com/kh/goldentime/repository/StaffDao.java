ce1969cd3ca664994093a29e563a57339d147c43
hmk813 <moonkyus@hanmail.net>
2022-12-06 11:05:54 +0900

    홈 jsp 등 디자인 구현중 ㅠㅠ

--- goldentime/src/main/java/com/kh/goldentime/repository/StaffDao.java
+++ goldentime/src/main/java/com/kh/goldentime/repository/StaffDao.java
@@ -6,11 +6,13 @@ import com.kh.goldentime.entity.StaffDto;
 
 public interface StaffDao {
 
-	public String loginCheck(StaffDto staffDto);//로그인
+	public String login(StaffDto staffDto);//로그인
 	void insert(StaffDto staffDto); //등록
 	List<StaffDto> list(); //목록
 	StaffDto selectOne(String staffId);//단일조회
 	boolean edit(StaffDto staffDto);//수정
 	boolean delete(String staffId);//삭제
+	
+	boolean changePassword(String staffId, String staffPw);//비밀번호 변경
 	
 }

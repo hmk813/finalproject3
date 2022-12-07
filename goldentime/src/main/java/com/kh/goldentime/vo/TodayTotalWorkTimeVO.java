8e62d7d72b5763fb6c611fd060b5272acce25ffc
hyunganom <dlguswo0504@naver.com>
2022-12-07 18:10:02 +0900

    오늘의 근무시간 구문

--- goldentime/src/main/java/com/kh/goldentime/vo/TodayTotalWorkTimeVO.java
+++ goldentime/src/main/java/com/kh/goldentime/vo/TodayTotalWorkTimeVO.java
@@ -9,11 +9,6 @@ import lombok.NoArgsConstructor;
 
 //출근시간과 퇴근시간을 비교하는 컬럼 추가
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
-public class AttendanceTotalWorkTimeVO {
-	private int attandanceNo; 
-	private String attendanceStaffId; 
-	private Date attendanceStartTime; 
-	private Date attendanceEndTime; 
-	private String attendanceWorkState; 
+public class TodayTotalWorkTimeVO {
 	private int workTime;
 }

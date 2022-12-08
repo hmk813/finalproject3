6cf8d66b6dec65fa793e20a8fa272df2b97fdd89
tjdwns6872 <tjdwns6872@naver.com>
2022-12-08 16:45:44 +0900

    예약 페이지 DAO, DTO, mapper Controller, RestController생성

--- goldentime/src/main/java/com/kh/goldentime/restcontroller/ReservationRestController.java
+++ goldentime/src/main/java/com/kh/goldentime/restcontroller/ReservationRestController.java
@@ -1,12 +1,25 @@
 package com.kh.goldentime.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.goldentime.entity.ReservationDto;
+import com.kh.goldentime.repository.ReservationDao;
 
 @CrossOrigin(origins = "http://127.0.0.1:5501")
 @RequestMapping("/rest")
 @RestController
 public class ReservationRestController {
 
+	@Autowired
+	private ReservationDao reservationDao;
+	
+	@PostMapping("reservation")
+	public void insert(@RequestBody ReservationDto reservationDto) {
+		reservationDao.insert(reservationDto);
+	}
 }

2a1025972cd292ea344df50355ddc6f38d367849
tjdwns6872 <tjdwns6872@naver.com>
2022-12-13 10:06:02 +0900

    예약

--- goldentime/src/main/java/com/kh/goldentime/entity/ReservationDto.java
+++ goldentime/src/main/java/com/kh/goldentime/entity/ReservationDto.java
@@ -1,9 +1,15 @@
 package com.kh.goldentime.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
 public class ReservationDto {
 	private int reservationNo;
 	private String reservationStaffId;
-	private int reservationPatientNumber;
+	private int reservationPatientNo;
 	private String reservationDate;
 	private String reservationContent;
 	private String reservationTime;
--- goldentime/src/main/java/com/kh/goldentime/restcontroller/ReservationRestController.java
+++ goldentime/src/main/java/com/kh/goldentime/restcontroller/ReservationRestController.java
@@ -1,7 +1,12 @@
 package com.kh.goldentime.restcontroller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kh.goldentime.entity.ReservationDto;
 import com.kh.goldentime.repository.ReservationDao;
 
-@CrossOrigin(origins = "http://127.0.0.1:5501")
+@CrossOrigin
 @RequestMapping("/rest")
 @RestController
 public class ReservationRestController {
@@ -18,8 +23,29 @@ public class ReservationRestController {
 	@Autowired
 	private ReservationDao reservationDao;
 	
-	@PostMapping("reservation")
+	@PostMapping("/reservation")
 	public void insert(@RequestBody ReservationDto reservationDto) {
 		reservationDao.insert(reservationDto);
 	}
+	
+	@GetMapping("/reservation")
+	public List<ReservationDto> list(){
+		return reservationDao.list();
+	}
+	
+	@GetMapping("/reservation/{reservationDate}")
+	public List<ReservationDto> dateList(@PathVariable String reservationDate){
+		return reservationDao.dateList(reservationDate);
+	}
+	
+	@DeleteMapping("/reservation/{reservationNo}")
+	public boolean delete(@PathVariable int reservationNo) {
+		return reservationDao.delete(reservationNo);
+	}
+	
 }
+
+
+
+
+

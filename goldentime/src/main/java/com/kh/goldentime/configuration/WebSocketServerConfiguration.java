9c8082c21aedfd217efbcfa17a4b4e086cd984be
JeongHwaP <pjm2327@naver.com>
2022-12-14 09:45:02 +0900

    웹소켓 구현중

--- goldentime/src/main/java/com/kh/goldentime/configuration/WebSocketServerConfiguration.java
+++ goldentime/src/main/java/com/kh/goldentime/configuration/WebSocketServerConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import com.kh.goldentime.websocket.MessageWebsocketServer;
+import com.kh.goldentime.websocket.JsonWebsocketServer;
 
 //웹소켓 서버 등록
 @Configuration//역할등록
@@ -15,12 +15,12 @@ public class WebSocketServerConfiguration implements WebSocketConfigurer {
 	
 	//서버등록을 위한 의존성
 	@Autowired
-	private MessageWebsocketServer messageUserWebsocketServer;
+	private JsonWebsocketServer jsonWebsocketServer;
 	
 	//registry에 만든 웹소켓 서버를 등록
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		//basicWebsocketServer를 registry에 등록
-		registry.addHandler(messageUserWebsocketServer, "/ws/message");//소켓 연결을 위한 주소
+		registry.addHandler(jsonWebsocketServer, "/ws/json");//소켓 연결을 위한 주소
 	}
 }
--- goldentime/src/main/java/com/kh/goldentime/controller/ChatController.java
+++ goldentime/src/main/java/com/kh/goldentime/controller/ChatController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/chat")
 public class ChatController {
 	
-	@GetMapping("/message")//WebSocketServerConfiguration에서 서버 등록한 주소와 동일한 이름
+	@GetMapping("/json")//WebSocketServerConfiguration에서 서버 등록한 주소와 동일한 이름
 	public String basic() {
-		return "chat/message";
+		return "chat/json";
 	}
 }

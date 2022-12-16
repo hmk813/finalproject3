abd7bd9aa2597c30ddddd369137e4d95d108c93b
tjdwns6872 <tjdwns6872@naver.com>
2022-12-16 09:39:33 +0900

    풀캘린더

--- goldentime/src/main/java/com/kh/goldentime/restcontroller/StaffRestController.java
+++ goldentime/src/main/java/com/kh/goldentime/restcontroller/StaffRestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kh.goldentime.entity.StaffDto;
 import com.kh.goldentime.repository.StaffDao;
 
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin
 @RestController
 @RequestMapping("/rest")
 public class StaffRestController {

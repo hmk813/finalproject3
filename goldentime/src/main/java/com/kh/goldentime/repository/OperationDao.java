eedb1748e204fb0a52fbd853b911193da7886c0a
REDCRAB11 <hellow9596@naver.com>
2022-12-20 16:51:38 +0900

    보류

--- goldentime/src/main/java/com/kh/goldentime/repository/OperationDao.java
+++ goldentime/src/main/java/com/kh/goldentime/repository/OperationDao.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.kh.goldentime.entity.PatientDto;
 import com.kh.goldentime.vo.OperationInformationVO;
 
 public interface OperationDao {
-	
-	//목록 조회 
-	List<OperationInformationVO>  selectList();
-	//단일 조회 
-//	OperationInformationVO selectOne(int operationNo);
-	//단일 조회2
-	OperationInformationVO selectOne(int patientNo);
-	
+   
+   //목록 조회 
+   List<OperationInformationVO>  selectList();
+   //단일 조회 
+//   OperationInformationVO selectOne(int operationNo);
+   //단일 조회2
+   OperationInformationVO selectOne(int patientNo);
+   List<PatientDto> selectPatientList();
+   
 }
--- goldentime/src/main/java/com/kh/goldentime/repository/OperationDaoImpl.java
+++ goldentime/src/main/java/com/kh/goldentime/repository/OperationDaoImpl.java
@@ -6,29 +6,34 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.goldentime.entity.PatientDto;
 import com.kh.goldentime.error.TargetNotFoundException;
 import com.kh.goldentime.vo.OperationInformationVO;
 
 @Repository
 public class OperationDaoImpl implements OperationDao{
 
-	@Autowired
-	private SqlSession sqlSession;
-	
-	@Override
-	public List<OperationInformationVO> selectList() {
-		return sqlSession.selectList("operation.list");
-	}
-	
-//	@Override
-//	public OperationInformationVO selectOne(int operationNo) {
-//		return sqlSession.selectOne("operation.one",operationNo);
-//	}
-	
-	@Override
-	public OperationInformationVO selectOne(int patientNo) {
-		return sqlSession.selectOne("operation.one",patientNo);
-	}
-	
+   @Autowired
+   private SqlSession sqlSession;
+   
+   @Override
+   public List<OperationInformationVO> selectList() {
+      return sqlSession.selectList("operation.list");
+   }
+   
+//   @Override
+//   public OperationInformationVO selectOne(int operationNo) {
+//      return sqlSession.selectOne("operation.one",operationNo);
+//   }
+   
+   @Override
+   public OperationInformationVO selectOne(int patientNo) {
+      return sqlSession.selectOne("operation.one",patientNo);
+   }
 
+   @Override
+   public List<PatientDto> selectPatientList() {
+      return sqlSession.selectList("patient.patientList");
+   }
+   
 }
